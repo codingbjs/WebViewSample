72ecf3cd40af13ff3dad97ef310f94c4e17c47e9
CDBJS <codingbjs@gmail.com>
2022-02-16 07:28:52 +0900

    WebChromeClient 추가

--- app/src/main/java/com/codingbjs/webviewsample/fragment/WebViewFragment.java
+++ app/src/main/java/com/codingbjs/webviewsample/fragment/WebViewFragment.java
@@ -1,9 +1,17 @@
 package com.codingbjs.webviewsample.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,7 +27,49 @@ public class WebViewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentWebViewBinding.inflate(inflater, container, false);
+
+        binding.domainEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                Log.d("onEditorAction", "onEditorAction() called with: textView = [" + textView + "], i = [" + i + "], keyEvent = [" + keyEvent + "]");
+                if(i == EditorInfo.IME_ACTION_NEXT) {
+                    String url = textView.getText().toString();
+                    if(!url.contains("https://") && !url.contains("http://")) {
+                        url = "https://" + url;
+                    }
+                    Log.d("url", url);
+                    binding.webView.loadUrl(url);
+                }
+                return false;
+            }
+        });
+
+        binding.webView.getSettings().setJavaScriptEnabled(true);
+
+        binding.webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+
+        binding.webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                binding.progressBar.setVisibility(View.VISIBLE);
+                if(newProgress == 100) {
+                    binding.progressBar.setVisibility(View.GONE);
+                }else {
+                    binding.progressBar.setProgress(newProgress);
+                }
+            }
+        });
+
+        binding.webView.loadUrl("https://www.daum.net");
+
         return binding.getRoot();
     }
+
+
 
 }
